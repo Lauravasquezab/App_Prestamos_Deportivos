87b3ee7d9dfcf8525a17cf5910c89f57545c895c
Laura Victoria <80783842+Lauravasquezab@users.noreply.github.com>
2023-10-17 07:47:16 -0500

    Primer FRM,Conexion y mas

--- App_PestamosDeportivos/Frm_Modulo_Usuarios.Designer.cs
+++ App_PestamosDeportivos/Frm_Modulo_Usuarios.Designer.cs
@@ -28,12 +28,255 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txt_Identificacion = new MaterialSkin.Controls.MaterialTextBox();
+            this.txt_Primernombre = new MaterialSkin.Controls.MaterialTextBox();
+            this.txtSegundoNombre = new MaterialSkin.Controls.MaterialTextBox();
+            this.txt_PrimerApellido = new MaterialSkin.Controls.MaterialTextBox();
+            this.txt_SegundoApellido = new MaterialSkin.Controls.MaterialTextBox();
+            this.txt_Contacto = new MaterialSkin.Controls.MaterialTextBox();
+            this.txtCorreo = new MaterialSkin.Controls.MaterialTextBox();
+            this.txtDireccion = new MaterialSkin.Controls.MaterialTextBox();
+            this.btn_Actualizar = new MaterialSkin.Controls.MaterialButton();
+            this.btn_Guardar = new MaterialSkin.Controls.MaterialButton();
+            this.btn_Cancelar = new MaterialSkin.Controls.MaterialButton();
+            this.SuspendLayout();
+            // 
+            // txt_Identificacion
+            // 
+            this.txt_Identificacion.AnimateReadOnly = false;
+            this.txt_Identificacion.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Identificacion.Depth = 0;
+            this.txt_Identificacion.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txt_Identificacion.Hint = "Identificacion";
+            this.txt_Identificacion.LeadingIcon = null;
+            this.txt_Identificacion.Location = new System.Drawing.Point(12, 108);
+            this.txt_Identificacion.MaxLength = 50;
+            this.txt_Identificacion.MouseState = MaterialSkin.MouseState.OUT;
+            this.txt_Identificacion.Multiline = false;
+            this.txt_Identificacion.Name = "txt_Identificacion";
+            this.txt_Identificacion.Size = new System.Drawing.Size(220, 50);
+            this.txt_Identificacion.TabIndex = 0;
+            this.txt_Identificacion.Text = "";
+            this.txt_Identificacion.TrailingIcon = null;
+            // 
+            // txt_Primernombre
+            // 
+            this.txt_Primernombre.AnimateReadOnly = false;
+            this.txt_Primernombre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Primernombre.Depth = 0;
+            this.txt_Primernombre.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txt_Primernombre.Hint = "Primer Nombre";
+            this.txt_Primernombre.LeadingIcon = null;
+            this.txt_Primernombre.Location = new System.Drawing.Point(252, 108);
+            this.txt_Primernombre.MaxLength = 50;
+            this.txt_Primernombre.MouseState = MaterialSkin.MouseState.OUT;
+            this.txt_Primernombre.Multiline = false;
+            this.txt_Primernombre.Name = "txt_Primernombre";
+            this.txt_Primernombre.Size = new System.Drawing.Size(244, 50);
+            this.txt_Primernombre.TabIndex = 1;
+            this.txt_Primernombre.Text = "";
+            this.txt_Primernombre.TrailingIcon = null;
+            // 
+            // txtSegundoNombre
+            // 
+            this.txtSegundoNombre.AnimateReadOnly = false;
+            this.txtSegundoNombre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSegundoNombre.Depth = 0;
+            this.txtSegundoNombre.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txtSegundoNombre.Hint = "Segundo Nombre";
+            this.txtSegundoNombre.LeadingIcon = null;
+            this.txtSegundoNombre.Location = new System.Drawing.Point(526, 108);
+            this.txtSegundoNombre.MaxLength = 50;
+            this.txtSegundoNombre.MouseState = MaterialSkin.MouseState.OUT;
+            this.txtSegundoNombre.Multiline = false;
+            this.txtSegundoNombre.Name = "txtSegundoNombre";
+            this.txtSegundoNombre.Size = new System.Drawing.Size(209, 50);
+            this.txtSegundoNombre.TabIndex = 2;
+            this.txtSegundoNombre.Text = "";
+            this.txtSegundoNombre.TrailingIcon = null;
+            // 
+            // txt_PrimerApellido
+            // 
+            this.txt_PrimerApellido.AnimateReadOnly = false;
+            this.txt_PrimerApellido.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_PrimerApellido.Depth = 0;
+            this.txt_PrimerApellido.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txt_PrimerApellido.Hint = "Primer Apellido";
+            this.txt_PrimerApellido.LeadingIcon = null;
+            this.txt_PrimerApellido.Location = new System.Drawing.Point(12, 191);
+            this.txt_PrimerApellido.MaxLength = 50;
+            this.txt_PrimerApellido.MouseState = MaterialSkin.MouseState.OUT;
+            this.txt_PrimerApellido.Multiline = false;
+            this.txt_PrimerApellido.Name = "txt_PrimerApellido";
+            this.txt_PrimerApellido.Size = new System.Drawing.Size(220, 50);
+            this.txt_PrimerApellido.TabIndex = 3;
+            this.txt_PrimerApellido.Text = "";
+            this.txt_PrimerApellido.TrailingIcon = null;
+            // 
+            // txt_SegundoApellido
+            // 
+            this.txt_SegundoApellido.AnimateReadOnly = false;
+            this.txt_SegundoApellido.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_SegundoApellido.Depth = 0;
+            this.txt_SegundoApellido.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txt_SegundoApellido.Hint = "Segundo Apellido";
+            this.txt_SegundoApellido.LeadingIcon = null;
+            this.txt_SegundoApellido.Location = new System.Drawing.Point(252, 191);
+            this.txt_SegundoApellido.MaxLength = 50;
+            this.txt_SegundoApellido.MouseState = MaterialSkin.MouseState.OUT;
+            this.txt_SegundoApellido.Multiline = false;
+            this.txt_SegundoApellido.Name = "txt_SegundoApellido";
+            this.txt_SegundoApellido.Size = new System.Drawing.Size(244, 50);
+            this.txt_SegundoApellido.TabIndex = 4;
+            this.txt_SegundoApellido.Text = "";
+            this.txt_SegundoApellido.TrailingIcon = null;
+            // 
+            // txt_Contacto
+            // 
+            this.txt_Contacto.AnimateReadOnly = false;
+            this.txt_Contacto.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Contacto.Depth = 0;
+            this.txt_Contacto.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txt_Contacto.Hint = "Contacto";
+            this.txt_Contacto.LeadingIcon = null;
+            this.txt_Contacto.Location = new System.Drawing.Point(526, 191);
+            this.txt_Contacto.MaxLength = 50;
+            this.txt_Contacto.MouseState = MaterialSkin.MouseState.OUT;
+            this.txt_Contacto.Multiline = false;
+            this.txt_Contacto.Name = "txt_Contacto";
+            this.txt_Contacto.Size = new System.Drawing.Size(209, 50);
+            this.txt_Contacto.TabIndex = 5;
+            this.txt_Contacto.Text = "";
+            this.txt_Contacto.TrailingIcon = null;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.AnimateReadOnly = false;
+            this.txtCorreo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCorreo.Depth = 0;
+            this.txtCorreo.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txtCorreo.Hint = "Correo";
+            this.txtCorreo.LeadingIcon = null;
+            this.txtCorreo.Location = new System.Drawing.Point(13, 248);
+            this.txtCorreo.MaxLength = 50;
+            this.txtCorreo.MouseState = MaterialSkin.MouseState.OUT;
+            this.txtCorreo.Multiline = false;
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(722, 50);
+            this.txtCorreo.TabIndex = 6;
+            this.txtCorreo.Text = "";
+            this.txtCorreo.TrailingIcon = null;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.AnimateReadOnly = false;
+            this.txtDireccion.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDireccion.Depth = 0;
+            this.txtDireccion.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txtDireccion.Hint = "Direccion";
+            this.txtDireccion.LeadingIcon = null;
+            this.txtDireccion.Location = new System.Drawing.Point(13, 305);
+            this.txtDireccion.MaxLength = 50;
+            this.txtDireccion.MouseState = MaterialSkin.MouseState.OUT;
+            this.txtDireccion.Multiline = false;
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(722, 50);
+            this.txtDireccion.TabIndex = 7;
+            this.txtDireccion.Text = "";
+            this.txtDireccion.TrailingIcon = null;
+            // 
+            // btn_Actualizar
+            // 
+            this.btn_Actualizar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn_Actualizar.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btn_Actualizar.Depth = 0;
+            this.btn_Actualizar.HighEmphasis = true;
+            this.btn_Actualizar.Icon = null;
+            this.btn_Actualizar.Location = new System.Drawing.Point(374, 376);
+            this.btn_Actualizar.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btn_Actualizar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btn_Actualizar.Name = "btn_Actualizar";
+            this.btn_Actualizar.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btn_Actualizar.Size = new System.Drawing.Size(109, 36);
+            this.btn_Actualizar.TabIndex = 8;
+            this.btn_Actualizar.Text = "Actualizar";
+            this.btn_Actualizar.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btn_Actualizar.UseAccentColor = false;
+            this.btn_Actualizar.UseVisualStyleBackColor = true;
+            // 
+            // btn_Guardar
+            // 
+            this.btn_Guardar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn_Guardar.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btn_Guardar.Depth = 0;
+            this.btn_Guardar.HighEmphasis = true;
+            this.btn_Guardar.Icon = null;
+            this.btn_Guardar.Location = new System.Drawing.Point(491, 376);
+            this.btn_Guardar.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btn_Guardar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btn_Guardar.Name = "btn_Guardar";
+            this.btn_Guardar.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btn_Guardar.Size = new System.Drawing.Size(88, 36);
+            this.btn_Guardar.TabIndex = 9;
+            this.btn_Guardar.Text = "Guardar";
+            this.btn_Guardar.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btn_Guardar.UseAccentColor = false;
+            this.btn_Guardar.UseVisualStyleBackColor = true;
+            // 
+            // btn_Cancelar
+            // 
+            this.btn_Cancelar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btn_Cancelar.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btn_Cancelar.Depth = 0;
+            this.btn_Cancelar.HighEmphasis = true;
+            this.btn_Cancelar.Icon = null;
+            this.btn_Cancelar.Location = new System.Drawing.Point(587, 376);
+            this.btn_Cancelar.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btn_Cancelar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btn_Cancelar.Name = "btn_Cancelar";
+            this.btn_Cancelar.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btn_Cancelar.Size = new System.Drawing.Size(96, 36);
+            this.btn_Cancelar.TabIndex = 10;
+            this.btn_Cancelar.Text = "Cancelar";
+            this.btn_Cancelar.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btn_Cancelar.UseAccentColor = false;
+            this.btn_Cancelar.UseVisualStyleBackColor = true;
+            // 
+            // Frm_Modulo_Usuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Cancelar);
+            this.Controls.Add(this.btn_Guardar);
+            this.Controls.Add(this.btn_Actualizar);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txt_Contacto);
+            this.Controls.Add(this.txt_SegundoApellido);
+            this.Controls.Add(this.txt_PrimerApellido);
+            this.Controls.Add(this.txtSegundoNombre);
+            this.Controls.Add(this.txt_Primernombre);
+            this.Controls.Add(this.txt_Identificacion);
+            this.Name = "Frm_Modulo_Usuarios";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private MaterialSkin.Controls.MaterialTextBox txt_Identificacion;
+        private MaterialSkin.Controls.MaterialTextBox txt_Primernombre;
+        private MaterialSkin.Controls.MaterialTextBox txtSegundoNombre;
+        private MaterialSkin.Controls.MaterialTextBox txt_PrimerApellido;
+        private MaterialSkin.Controls.MaterialTextBox txt_SegundoApellido;
+        private MaterialSkin.Controls.MaterialTextBox txt_Contacto;
+        private MaterialSkin.Controls.MaterialTextBox txtCorreo;
+        private MaterialSkin.Controls.MaterialTextBox txtDireccion;
+        private MaterialSkin.Controls.MaterialButton btn_Actualizar;
+        private MaterialSkin.Controls.MaterialButton btn_Guardar;
+        private MaterialSkin.Controls.MaterialButton btn_Cancelar;
     }
 }
--- App_PestamosDeportivos/cls_Modulo_Usuario.cs
+++ App_PestamosDeportivos/cls_Modulo_Usuario.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace App_PestamosDeportivos
 {
@@ -19,6 +22,30 @@ namespace App_PestamosDeportivos
         private String str_sexo; 
         cls_Conexion objConectar = new cls_Conexion();
 
-
-    }
+        public void fnt_guardar(string id,string pnombre,string snombre,string papellido, string sapellidoString,string contacto,String corrreo,string direccion,string sexo)
+        {
+            if(id.Equals("")||pnombre.Equals("")||snombre.Equals("")||papellido.Equals("")||sapellido.equals("")||contacto.Equals("")||str_correo.Equals("")||direccion.Equals(""))
+            {
+                MessageBox.Show("Debe ingresar todos los datos","Registro",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else
+            {
+                SqlCommand con = new SqlCommand("SP_Registrar_Personas", objConectar.connection);
+                con.CommandType = CommandType.StoredProcedure;
+                con.Parameters.AddWithValue("@id", id);
+                con.Parameters.AddWithValue("@p_nombre", pnombre);
+                con.Parameters.AddWithValue("@s_nombre", snombre);
+                con.Parameters.AddWithValue("@p_apellido", papellido);
+                con.Parameters.AddWithValue("@s_apellido", sapellido);
+                con.Parameters.AddWithValue("@contacto", contacto);
+                con.Parameters.AddWithValue("@direccion", direccion);
+                con.Parameters.AddWithValue("@sexo", sexo);
+                con.Parameters.AddWithValue("@correo", correo);
+                objConectar.connection.Open();
+                con.ExecuteNonQuery();
+                objConectar.connection.Close();
+                MessageBox.Show("Datos registrados con éxito", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    } 
 }
