a7f8eba2137c6acbe4d7803c94a242f01828e639
Laura Victoria <80783842+Lauravasquezab@users.noreply.github.com>
2023-10-17 07:19:26 -0500

    Conexión a la base de datos

--- App_PestamosDeportivos/cls_Modulo_Usuario.cs
+++ App_PestamosDeportivos/cls_Modulo_Usuario.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace App_PestamosDeportivos
 {
-    internal class cls_Modulo_Usuario
+    public class cls_Modulo_Usuario
     {
+        private String str_id;
+        private String str_pnombre;
+        private String str_snombre;
+        private String str_papellido;
+        private String str_sapellido;
+        private String str_contacto;
+        private String str_correo;
+        private String str_direccion;
+        private String str_sexo; 
+        cls_Conexion objConectar = new cls_Conexion();
+
+
     }
 }
